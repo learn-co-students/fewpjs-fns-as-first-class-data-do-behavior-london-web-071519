3adf9bb0b163a978dce3f003cf5d9d07197a59d6
Harry Chopra <harrychopra@gmail.com>
2019-08-29 12:52:34 +0100

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(input){
+ let num = parseInt(input, 10);
+  if (num < 12) return "Good Morning"
+  if ( num >= 17 ) return "Good Evening"
+  else return "Good Afternoon"
+}
+
+
 /* Write your implementation of displayMessage() */
+function displayMessage(input){
+  document.querySelector("#greeting").innerText = input;
+}
